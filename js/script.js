35ed5125e9307b4a222b4a1a8cea189650ce92f3
farzinGhaheri <f.ghaheri@gmail.com>
2021-02-14 19:04:20 +0800

    Done

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function getDate() {
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+document.getElementById("selectDate").innerHTML = date
+}
+
+function changeTabs(evt, item) {
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tabContent")
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabLinks")
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
