b6a44a342c3031472b25c57ff27052b0bad86bf0
Farzin Ghaheri <farzin.ghaheri@insuradar.com>
2021-08-28 02:59:11 +0800

    add theme color

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 document.getElementById("selectDate").innerHTML = date;
 }
 function changeTabs(evt, item) {
-    var i, tabContent, tabLinks, checkBio;
+    var i, tabContent, tabLinks;
     tabContent = document.getElementsByClassName("tabContent")
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
@@ -19,4 +19,18 @@ function changeTabs(evt, item) {
     if(evt.currentTarget.className === 'navbar-brand tabLinks active'){
     document.getElementById('bioText').style.display="inline-flex";
     }
+}
+
+function changeColor(data){
+   if(data === 'on'){
+   let check = document.getElementById('getBody');
+   let changeValue = document.getElementById('flexSwitchCheckDefault');
+   check.classList.add('dark-color');
+   changeValue.setAttribute('value', 'off');
+   }else{
+    let check = document.getElementById('getBody');
+    check.classList.remove('dark-color');
+    let changeValue = document.getElementById('flexSwitchCheckDefault');
+    changeValue.setAttribute('value', 'on');
+   }
 }
